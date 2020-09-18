bb35566d7eff0f91e25f02332bef75d71e6eac5f
Fe-Nik-S <mikifeynman@gmail.com>
2020-09-18 21:57:46 +0300

    Added packages and env for babel

--- js/react/redux_for_beginners_v1/webpack.config.js
+++ js/react/redux_for_beginners_v1/webpack.config.js
@@ -6,6 +6,7 @@ module.exports = {
     devtool: 'cheap-module-eval-source-map',
     entry: [
         'webpack-hot-middleware/client',
+        'babel-polyfill',
         './src/index'
     ],
     output: {
@@ -17,5 +18,26 @@ module.exports = {
         new webpack.optimize.OccurenceOrderPlugin(),
         new webpack.HotModuleReplacementPlugin(),
         new webpack.NoErrorsPlugin()
-    ]
+    ],
+    module: {
+        preLoaders: [
+          {
+            test: /\.js$/,
+            loaders: ['eslint'],
+            include: [
+              path.resolve(__dirname, "src"),
+            ],
+          }
+        ],
+        loaders: [
+          {
+            loaders: ['react-hot', 'babel-loader'],
+            include: [
+              path.resolve(__dirname, "src"),
+            ],
+            test: /\.js$/,
+            plugins: ['transform-runtime'],
+          }
+        ]
+    }
 }
