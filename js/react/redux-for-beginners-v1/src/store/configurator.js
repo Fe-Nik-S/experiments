db97c970baf7de6a7cf4fa663e500ce8efb6cca9
Fe-Nik-S <mikifeynman@gmail.com>
2020-09-23 21:57:32 +0300

    Changes custom logger to default redux-logger

--- js/react/redux-for-beginners-v1/src/store/configurator.js
+++ js/react/redux-for-beginners-v1/src/store/configurator.js
@@ -1,18 +1,19 @@
 
 import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducers';
-import { ping } from '../enhancers/ping'
+import { createLogger } from 'redux-logger';
 
 
 export default function configureStore(initialState) {
-    const store = createStore(rootReducer, initialState, applyMiddleware(ping));
+    const logger = createLogger();
+    const store = createStore(rootReducer, initialState, applyMiddleware(logger));
 
     if (module.hot) {
         module.hot.accept('../reducers', () => {
             const nextRootReducer = require('../reducers')
-            store.replaceReducer(nextRootReducer)
+            store.replaceReducer(nextRootReducer);
         })
     }
 
-    return store
+    return store;
 }
