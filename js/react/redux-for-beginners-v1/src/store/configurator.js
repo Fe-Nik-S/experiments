4f82e86ff367730d27ce2820882441b86d87df0e
Fe-Nik-S <mikifeynman@gmail.com>
2020-09-23 21:53:47 +0300

    Added custom middlware

--- js/react/redux-for-beginners-v1/src/store/configurator.js
+++ js/react/redux-for-beginners-v1/src/store/configurator.js
@@ -1,10 +1,11 @@
 
-import { createStore } from 'redux'
-import rootReducer from '../reducers'
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer from '../reducers';
+import { ping } from '../enhancers/ping'
 
 
 export default function configureStore(initialState) {
-    const store = createStore(rootReducer, initialState)
+    const store = createStore(rootReducer, initialState, applyMiddleware(ping));
 
     if (module.hot) {
         module.hot.accept('../reducers', () => {
