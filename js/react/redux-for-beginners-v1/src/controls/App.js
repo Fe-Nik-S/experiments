fe8aed29b39bb309792e677e0f1dba5daad2fd78
Fe-Nik-S <mikifeynman@gmail.com>
2020-09-23 14:54:01 +0300

    Refactoring. Separate controls and containers

--- js/react/redux-for-beginners-v1/src/controls/App.js
+++ js/react/redux-for-beginners-v1/src/controls/App.js
@@ -1,12 +1,17 @@
 
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import User from '../controls/User'
+import Page from '../controls/Page';
 
 
 class App extends Component {
     render() {
+        const { user, page } = this.props;
         return <div>
             <p>Redux tutorial</p>
+            <User name={user.name} />
+            <Page photos={page.photos} year={page.year} />
         </div>
     }
 }
