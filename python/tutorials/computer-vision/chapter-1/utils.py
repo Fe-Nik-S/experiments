eceb9779e71995c701b87b608d6d709e9823766b
Fe-Nik-S <mikifeynman@gmail.com>
2020-12-15 01:09:30 +0300

    CV. Added small func to find images

--- python/tutorials/computer-vision/chapter-1/utils.py
+++ python/tutorials/computer-vision/chapter-1/utils.py
@@ -1,5 +1,6 @@
 
 import logging
+import os
 
 
 def save_as(pil_im, output_file_name, extension="jpg"):
@@ -8,3 +9,12 @@ def save_as(pil_im, output_file_name, extension="jpg"):
         pil_im.save(output_file)
     except Exception as ex:
         logging.error("Error during convert image to '{}': {}".format(extension, ex))
+
+
+def get_files_by_extension(dir_path, extension="jpg"):
+    if not os.path.exists(dir_path):
+        logging.error("Path to dir '{}' not found".format(dir_path))
+    all_files = os.listdir(dir_path)
+    matched_files = filter(lambda file_name: file_name.endswith('.{}'.format(extension)), all_files)
+    return [os.path.abspath(os.path.join(dir_path, file_name)) for file_name in matched_files]
+
