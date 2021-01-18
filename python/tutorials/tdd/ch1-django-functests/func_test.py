859052b55d8eca9cfb4d8cedef23525c452eb653
Fe-Nik-S <mikifeynman@gmail.com>
2021-01-19 01:17:01 +0300

    TDD. Initial commit. Added django test project

--- python/tutorials/tdd/ch1-django-functests/func_test.py
+++ python/tutorials/tdd/ch1-django-functests/func_test.py
@@ -1,10 +1,18 @@
 
 from selenium import webdriver
+import logging
 
 
-PORT = 8080
+PORT = 8000
+
+
+logging.basicConfig(
+    level=logging.INFO
+)
+
 
 browser = webdriver.Firefox()
 browser.get('http://localhost:{}'.format(PORT))
 
 assert 'Django' in browser.title
+logging.info("Tests passed successfully...")
